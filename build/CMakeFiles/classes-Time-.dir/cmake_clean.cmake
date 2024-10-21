94a1b895c6bb81510f6bf2b5f4bc87e193a90aeb
arina hasandjanova <arinahasandjanova@gmail.com>
2024-10-21 15:12:08 +0400

    lion

---
+++ build/CMakeFiles/classes-Time-.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classes-Time-.dir/main.cpp.o"
+  "CMakeFiles/classes-Time-.dir/main.cpp.o.d"
+  "classes-Time-"
+  "classes-Time-.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/classes-Time-.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/classes-Time-.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for classes-Time-.
+# This may be replaced when dependencies are built.
